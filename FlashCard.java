48eed5a09f682990b37f504e7d2af99970411214
Benjamin Leach <benjaminstephenleach@gmail.com>
2020-12-10 18:32:03 -0600

    added getter and setter methods to flashcard class

--- FlashCard.java
+++ FlashCard.java
@@ -1,13 +1,38 @@
-public class FlashCard 
+class FlashCard 
 {
 	private String question;
 	private String[] choices;
 	private String answer;
 
-	public FlashCard(String question, String[] choices, String answer)
+	public FlashCard(String question, String[] choices)
 	{
 		this.question = question;
 		this.choices = choices;
-		this.answer = answer;
 	} 
-}
+} // end class FlashCard
+
+class Choice 
+{
+	private String choice;
+	private boolean correctAnswer;
+
+	public void setChoice(String choice)
+	{
+		this.choice = choice;	
+	}
+
+	public void setCorrectAnswer(boolean correctAnswer)
+	{
+		this.correctAnswer = correctAnswer;
+	}
+
+	public String getChoice()
+	{
+		return this.choice;
+	}	
+
+	public boolean getCorrectAnswer()
+	{
+		return this.correctAnswer;
+	}	
+} // end class Choice
